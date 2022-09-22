b692776f81af9564e9bf0c21d3d4ff670fc0c248
B1zy <111041043+B1zy@users.noreply.github.com>
2022-09-22 09:03:11 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,55 +4,45 @@ namespace Number_guessing_game
 {
     class Program
     {
-       
 
-         public static void  Main(string[] args)
+
+        public static void Main(string[] args)
         {
 
         start:
 
-            
+
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             int Guesses = 0;
             int Guess;
-            int winCount = 0;
+            string youWonText = @"
+_____.___.               __      __              
+\__  |   | ____  __ __  /  \    /  \____   ____  
+ /   |   |/  _ \|  |  \ \   \/\/   /  _ \ /    \ 
+ \____   (  <_> )  |  /  \        (  <_> )   |  \
+ / ______|\____/|____/    \__/\  / \____/|___|  /
+ \/                            \/             \/ 
+";
+
             Random rnd = new Random();
             int Generated = rnd.Next(1, 100);
-            Console.WriteLine(Generated);
+            //Console.WriteLine(Generated);
 
             Console.WriteLine("Eine zufällige Zahl zwischen 1-100 wurde Generiert");
             Console.WriteLine("Probieren Sie es zu erraten");
-           
-            
+            Console.WriteLine(Generated);
+
+
             do
             {
 
 
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                string strGuess =Console.ReadLine();
+                string strGuess = Console.ReadLine();
                 Guess = Convert.ToInt32(strGuess);
                 Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine(winCount + ":Wincount");
-                if (winCount == 5)
-                {
-                 
-                    Console.WriteLine("Wollen Sie ein schwieriegeres Modus spielen? [Ja|Nein]");
-                    string winCountInput = Console.ReadLine();
-                    if (winCountInput == "Ja"|| winCountInput == "ja")
-                    {
-                        Random rnd2 = new Random();
-                        int Generated2 = rnd2.Next(1, 1000);
-                        Console.WriteLine(Generated2);
-                        Console.WriteLine("Eine zufällige Zahl zwischen 1-1000 wurde Generiert");
-                        Console.WriteLine("Probieren Sie es zu erraten");
 
 
-                    }
-                    if (winCountInput == "Nein" || winCountInput == "nein") 
-                    {
-                        Console.WriteLine("Schade");
-                    }
-                }
 
 
                 if (Guess > 100)
@@ -64,39 +54,40 @@ namespace Number_guessing_game
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Probieren sie höcher ↑");
-                    
+
                     Guesses++;
                 }
-                else if ( Guess > Generated) 
+                else if (Guess > Generated)
                 {
                     Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine("Probieren sie tiefer ↓");
-               
+
                     Guesses++;
                 }
 
                 else if (Guess == Generated)
-                    
+
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("Gut gemacht, Sie haben es geschafft");
                     Console.WriteLine("Sie haben " + Guesses + " Versuche gebraucht");
+                    Console.WriteLine(youWonText);
 
 
-                    
+
                     Console.WriteLine("Möchten Sie nochmal spielen?[Ja|Nein]");
 
                     string restart = Console.ReadLine();
 
-                    if (restart == "ja"|| restart == "Ja")
+                    if (restart == "ja" || restart == "Ja")
                     {
-                        winCount++;
-                        Console.WriteLine(winCount);
-                        goto start ;
-                        
+
+                        goto start;
+
                     }
-                    if (restart == "nein"|| restart == "Nein")
+                    if (restart == "nein" || restart == "Nein")
                     {
+                        
                         Environment.Exit(1);
 
                     }
@@ -108,8 +99,8 @@ namespace Number_guessing_game
 
 
                 }
-            } while (Guess != Generated|| winCount != 5 );
-            
+            } while (Guess != Generated);
+
 
 
 
@@ -117,4 +108,3 @@ namespace Number_guessing_game
         }
     }
 }
-//
